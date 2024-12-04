520a2711f025c29d5b1c32e7899dbad077abddcd
Ivan Cheban <ivancheban@gmail.com>
2024-12-04 18:04:24 +0200

    removed scheduler

--- functions/scheduled-telegram-bot.js
+++
@@ -1,7 +0,0 @@
-const { schedule } = require('@netlify/functions');
-const { handler } = require('./telegram-bot');
-
-// Run every hour
-const scheduledFunction = schedule('0 * * * *', handler);
-
-module.exports.handler = scheduledFunction;
